637f2ccf9be47ed7f4cdf0a7dffd361a9b59b3ce
Flixjod <144277345+Flixjod@users.noreply.github.com>
2024-09-30 23:36:50 +0530

    Update and rename Login.py66 to Login.py

--- FLiX/Login.py
+++ FLiX/Login.py
@@ -25,6 +25,31 @@ def get(obj, key, default=None):
     except:
         return default
 
+@Client.on_message(filters.command(["logout"]))
+async def logout(client: Client, message: Message):
+    if not await is_member(client, message.from_user.id):
+        
+        await client.send_message(
+            chat_id=message.chat.id,
+            text=f"👋 ʜɪ {message.from_user.mention}, ʏᴏᴜ ᴍᴜsᴛ ᴊᴏɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴜsᴇ ᴍᴇ.",
+            reply_markup=InlineKeyboardMarkup([[
+                InlineKeyboardButton("ᴊᴏɪɴ ❤️", url=FSUB_INV_LINK)
+            ]]),
+            reply_to_message_id=message.id  
+        )
+        return
+        
+    user_data = database.sessions.find_one({"user_id": message.chat.id})
+    if user_data is None or not user_data.get('logged_in', False):
+        await message.reply("**You are not logged in! Please /login first.**")
+        return
+    data = {
+        'logged_in': False,
+        'session': None,
+        '2FA': None
+    }
+    database.sessions.update_one({'_id': user_data['_id']}, {'$set': data})
+    await message.reply("**Logout Successfully** ♦")
 
 
 @Client.on_message(filters.command("login") & filters.private)
