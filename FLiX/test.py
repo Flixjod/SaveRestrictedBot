040c2fc141f4af3934a063b4af6bbec185229f47
Flixjod <144277345+Flixjod@users.noreply.github.com>
2024-09-30 22:37:50 +0530

    Update test.py

--- FLiX/test.py
+++ FLiX/test.py
@@ -6,10 +6,6 @@ from database.db import database
 
 @Client.on_message(filters.command("test") & filters.private)
 async def test(client: Client, message: Message):
-    user_data = database.find_one({"user_id": message.from_user.id})
     
-    if user_data is None:
-        await message.reply("**You are not logged in! Please /login first.**")
-        return
 
     await message.reply("**Test command received! You are logged in.**")
