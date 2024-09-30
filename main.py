9b3d177ba4837ca21ba61999aabfb3422d4b6c43
Flixjod <144277345+Flixjod@users.noreply.github.com>
2024-09-30 23:06:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            plugins=dict(root="FLiX"),
+            plugins={'root':'FLiX'},
             workers=50,
             sleep_threshold=10
         )
