ce8ba863312d112aa4817d270645e8321521d90b
Flixjod <144277345+Flixjod@users.noreply.github.com>
2024-10-04 10:53:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ DB_URI = os.environ.get("DB_URI", "")
 LOGS_CHAT_ID = int(os.environ.get("LOGS_CHAT_ID", ""))
 
 #Force Sub Channel ID
-FSUB_ID = int(os.environ.get("FSUB_ID", ""))
+FSUB_ID = os.environ.get("FSUB_ID", "")
 if FSUB_ID:
     FSUB_ID = int(FSUB_ID)  # Convert to int if it's not empty
 else:
